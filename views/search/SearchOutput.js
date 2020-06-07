e95325fd6d676bef552e7e96f63a4fab38e28c75
laura-davis <laura.davis@sky.uk>
2020-06-07 21:25:26 +0100

    Added steps to SearchOutput page

--- views/search/SearchOutput.js
+++ views/search/SearchOutput.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import {Button, Card, TextInput, Headline, ProgressBar, Avatar} from 'react-native-paper';
-import Icon from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { View, Text } from 'react-native';
-
 const SearchOutput = () => (
     <Card>
         <Headline>Results</Headline>
@@ -10,13 +9,33 @@ const SearchOutput = () => (
                     <Text>&nbsp;</Text>
         <ProgressBar/>
                     <Text>&nbsp;</Text>
-
             </View>
 
             <View style={{justifyContent: "center", alignItems: "center"}}>
         <Avatar.Text size={50} label="1" style={{alignItems: 'center', justifyContent:'center'}} />
-</View>
-        </Card>
+                <Icon name='dots-vertical' size={25}
+                />
+                <Text>Received by Gonzales OTT</Text>
+                <Text>09.03am 10th May 2020</Text>
+                <Icon name='dots-vertical' size={25}
+                />
+                <Avatar.Text size={50} label="2" style={{alignItems: 'center', justifyContent:'center'}} />
+                <Icon name='dots-vertical' size={25}
+                />
+                <Text>Received by Gonzales DTH</Text>
+                <Text>09.05am 10th May 2020</Text>
+                <Icon name='dots-vertical' size={25}
+                />
+                <Avatar.Text size={50} label="3" style={{alignItems: 'center', justifyContent:'center'}} />
+                <Icon name='dots-vertical' size={25}
+                />
+                <Text>Sent to PD / PMP</Text>
+                <Text>09.10am 10th May 2020</Text>
+                <Icon name='dots-vertical' size={25}
+                />
+            </View>
+
+    </Card>
 );
 
 export default SearchOutput;
